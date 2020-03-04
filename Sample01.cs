616a65365258d51016c5e1e843ab7cec780c8a64
hayakawa1234 <61731486+hayakawa1234@users.noreply.github.com>
2020-03-04 13:15:37 +0900

    新しい演習

--- Sample01.cs
+++ Sample01.cs
@@ -7,7 +7,7 @@ public class Sample01 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("Hello World!!!");  
+       Debug.Log("Hello World!");  
     }
 
     
